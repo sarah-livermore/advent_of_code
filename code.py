6dccf2d8156f9babe1a0a00dffe7a9fa8eb2a589
sarah-livermore <sarah@sarahlivermore.com>
2024-12-07 09:46:10 +0000

    added day 2 part 1 code (but wrong answer)

--- code.py
+++ code.py
@@ -20,3 +20,22 @@ id_diffs = [abs(id_1-id_2) for id_1,id_2 in zip(first_ids, second_ids)]
 # total distance
 total_dist = sum(id_diffs)
 print('Total distance between all ID pairs: ', total_dist)
+
+# DAY 2, PART 1 - LEVELS
+
+def file_reader(file):
+    with open(file) as f:
+        for line in f:
+            levels = line.split(' ')
+            levels = [int(l) for l in levels]
+            diffs = [t-s for s,t in zip(levels, levels[1:])]
+            yield diffs
+
+invalid_diffs = 0
+for diffs in file_reader('input2.txt'):
+    print('diff in question', diffs)
+    if False in [abs(i)<=3 and abs(i)>0 for i in diffs] or len(set(i<0 for i in diffs)) > 1:
+        print('invalid')
+        invalid_diffs+=1
+
+print('num invalid levels', invalid_diffs)
